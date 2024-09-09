b8708f46072d85e3b98c207ea9d82b04c567afde
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-09-09 13:31:58 -0600

    Develop styling for tour dates listings section

--- src/components/Dates/Dates.jsx
+++ src/components/Dates/Dates.jsx
@@ -1,14 +1,19 @@
+import './Dates.css'
+
 function Dates() {
 
     return (
-        <div className="tour-dates">
-            <h1>UPCOMING TOUR DATES</h1>
-            <div className="tour-listing">
-                <h3 className="tour-city">Loveland, CO</h3>
-                <p>May 11, 2024</p>
-                <p>Rialto Theater</p>
-                <hr></hr>
+        <div className="tours">
+            <div className='tour-title'>
+                <h1>UPCOMING TOUR DATES</h1>
             </div>
+            <div className="tour-listing">
+                <h3 className="tour-city">LOVELAND, CO</h3>
+                <p className="tour-date">May 11, 2024</p>
+                <p className="tour-location">Rialto Theater</p>
+                <button className="tour-button-style">LEARN MORE</button>
+            </div>
+            <hr className='listing-line'></hr>
         </div>
     )
 }
