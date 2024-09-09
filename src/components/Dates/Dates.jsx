3b376d02f9d3a8e767aeafba1f3af325c6b99e4e
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-09-09 13:40:59 -0600

    Finish tour dates section, still need to program buttons

--- src/components/Dates/Dates.jsx
+++ src/components/Dates/Dates.jsx
@@ -14,6 +14,27 @@ function Dates() {
                 <button className="tour-button-style">LEARN MORE</button>
             </div>
             <hr className='listing-line'></hr>
+            <div className="tour-listing">
+                <h3 className="tour-city">RIFLE, CO</h3>
+                <p className="tour-date">November 23, 2024</p>
+                <p className="tour-location">Ute Theater</p>
+                <button className="tour-button-style">LEARN MORE</button>
+            </div>
+            <hr className='listing-line'></hr>
+            <div className="tour-listing">
+                <h3 className="tour-city">BRIGHTON, CO</h3>
+                <p className="tour-date">January 18, 2025</p>
+                <p className="tour-location">Armory Brighton</p>
+                <button className="tour-button-style">LEARN MORE</button>
+            </div>
+            <hr className='listing-line'></hr>            
+            <div className="tour-listing">
+                <h3 className="tour-city">BROOMFIELD, CO</h3>
+                <p className="tour-date">March 23, 2025</p>
+                <p className="tour-location">Broomfield Auditorium</p>
+                <button className="tour-button-style">LEARN MORE</button>
+            </div>
+            <hr className='listing-line'></hr>
         </div>
     )
 }
