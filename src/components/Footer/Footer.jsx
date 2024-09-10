721da74d0012c77b851fa8b4aa50f727b795bf19
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-09-09 21:16:29 -0600

    Update social links, open new tab when redirect to socail media page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,21 +8,30 @@ function Footer() {
     return (
         <footer>
             <div className="social-links" aria-label="Social media links">
-                <a className="facebook" href="https://www.facebook.com/people/LAYLA-and-other-assorted-Eric-Clapton-songs-Musical-Showcase/61562647367534/">
+                <a className="facebook"
+                    href="https://www.facebook.com/people/LAYLA-and-other-assorted-Eric-Clapton-songs-Musical-Showcase/61562647367534/"
+                    target='_blank'
+                >
                     <img 
-                    className="icon"
-                    src={facebook}
-                    alt="Production group facebook profile link"
+                        className="link"
+                        src={facebook}
+                        alt="Production group facebook profile link"
                     />
                 </a>
-                <a className="instagram" href="">
+                <a className="instagram" 
+                    href="" 
+                    target='_blank'
+                >
                     <img 
-                    className="icon"
-                    src={instagram}
-                    alt="Production group instagram profile link"
+                        className="link"
+                        src={instagram}
+                        alt="Production group instagram profile link"
                     />
                 </a>
-                <a className="youtube" href="https://www.youtube.com/channel/UCV9foKeW3pNIOOCOqq6_8Yg">
+                <a className="link" 
+                    href="https://www.youtube.com/channel/UCV9foKeW3pNIOOCOqq6_8Yg"
+                    target='_blank'
+                    >
                     <img 
                     className="icon"
                     src={youtube}
