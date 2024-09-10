21eedc7630826497d0af1a594bae800509b22e92
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-09-09 19:16:21 -0600

    Finish basic structure of cast component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,8 +8,16 @@ function About() {
     // these are a placeholder
     return (
         <div className="about" aria-label="About the production section"> 
-            <img className="about-top-img" src={aboutTopImg} alt="Guitarists and drummer preforming live" />
-            <img className="about-header-img" src={aboutHeaderImg} alt="Image text reads: a celebration of a musical icon"/>
+            <img
+                className="about-top-img"
+                src={aboutTopImg}
+                alt="Guitarists and drummer preforming live" 
+                />
+            <img
+                className="about-header-img"
+                src={aboutHeaderImg}
+                alt="Image text reads: a celebration of a musical icon"
+                />
             <p className="about-text"><span className="gray-text">LAYLA and other assorted Eric Clapton songs </span> is a musical homage to one of the most influential guitarists and songwriters of our time, Eric Clapton. The show is presented in two acts–one acoustic and one electric–paying tribute to his life and musical career through story, multimedia and set to the beat of his popular and beloved hits including “Layla,” “Tears In Heaven,” “Change the World,” “Cocaine” and so many more.</p>
             <img className="about-bottom-img" src={aboutBottomImg} />
         </div>
--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,16 +1,66 @@
 import castPlaceholderImg from '../../assets/cast-ph-img.jpg'
+import castBottomImg from '../../assets/cast-bottom.png'
 import './Cast.css'
+
+// Need new bottom of page image
 
 function Cast() {
 
     return (
         <div className="cast" aria-label="cast members section">
             <h1>Cast</h1>
-            <div className="cast-card"
-                aria-label="list of cast members and descriptions">
-                <img className="cast-img" src={castPlaceholderImg}/>
-                <h3><a>Person Name</a></h3>
+            <div className="cast-card-area">
+                <div className="cast-card"
+                    aria-label="list of cast members and descriptions">
+                    <img className="cast-img" src={castPlaceholderImg}/>
+                    <h3><a>Person Name</a></h3>
+                </div>
+                <div className="cast-card"
+                    aria-label="list of cast members and descriptions">
+                    <img className="cast-img" src={castPlaceholderImg}/>
+                    <h3><a>Person Name</a></h3>
+                </div>
+                <div className="cast-card"
+                    aria-label="list of cast members and descriptions">
+                    <img className="cast-img" src={castPlaceholderImg}/>
+                    <h3><a>Person Name</a></h3>
+                </div>
+                <div className="cast-card"
+                    aria-label="list of cast members and descriptions">
+                    <img className="cast-img" src={castPlaceholderImg}/>
+                    <h3><a>Person Name</a></h3>
+                </div>
+                <div className="cast-card"
+                    aria-label="list of cast members and descriptions">
+                    <img className="cast-img" src={castPlaceholderImg}/>
+                    <h3><a>Person Name</a></h3>
+                </div>
+                <div className="cast-card"
+                    aria-label="list of cast members and descriptions">
+                    <img className="cast-img" src={castPlaceholderImg}/>
+                    <h3><a>Person Name</a></h3>
+                </div>
+                <div className="cast-card"
+                    aria-label="list of cast members and descriptions">
+                    <img className="cast-img" src={castPlaceholderImg}/>
+                    <h3><a>Person Name</a></h3>
+                </div>                <div className="cast-card"
+                    aria-label="list of cast members and descriptions">
+                    <img className="cast-img" src={castPlaceholderImg}/>
+                    <h3><a>Person Name</a></h3>
+                </div>                <div className="cast-card"
+                    aria-label="list of cast members and descriptions">
+                    <img className="cast-img" src={castPlaceholderImg}/>
+                    <h3><a>Person Name</a></h3>
+                </div>
             </div>
+            <img 
+                className="cast-bottom-img"
+                src={castBottomImg}
+                alt="Image of live performance"
+            />
+            <hr className='orange-line'></hr>
+            
         </div>
     )
 }
