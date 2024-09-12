858ba67579a1db3c5025ec5bb70700dd6730a2ba
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-09-12 16:34:19 -0600

    Update learn more buttons with a tags, link to external show websites if available

--- src/components/Dates/Dates.jsx
+++ src/components/Dates/Dates.jsx
@@ -15,28 +15,48 @@ function Dates() {
                 <h3 className="tour-city">LOVELAND, CO</h3>
                 <p className="tour-date">May 11, 2024</p>
                 <p className="tour-location">Rialto Theater</p>
-                <button className="tour-button-style">LEARN MORE</button>
+                <a 
+                    className="tour-button-style" 
+                    target='_blank'
+                >
+                    <button className="tour-button">LEARN MORE</button>
+                </a>
             </div>
             <hr className='listing-line'></hr>
             <div className="tour-listing" aria-label="individual tour date listing">
                 <h3 className="tour-city">RIFLE, CO</h3>
                 <p className="tour-date">November 23, 2024</p>
                 <p className="tour-location">Ute Theater</p>
-                <button className="tour-button-style">LEARN MORE</button>
+                <a 
+                    className="tour-button-style" 
+                    href="https://utetheater.com/event/forever-man-layla-other-assorted-eric-clapton-songs/" target='_blank'
+                >
+                    <button className="tour-button">LEARN MORE</button>
+                </a>
             </div>
             <hr className='listing-line'></hr>
             <div className="tour-listing" aria-label="individual tour date listing">
                 <h3 className="tour-city">BRIGHTON, CO</h3>
                 <p className="tour-date">January 18, 2025</p>
                 <p className="tour-location">Armory Brighton</p>
-                <button className="tour-button-style">LEARN MORE</button>
+                <a 
+                    className="tour-button-style" 
+                    target='_blank'
+                >
+                    <button className="tour-button">LEARN MORE</button>
+                </a>
             </div>
             <hr className='listing-line'></hr>            
             <div className="tour-listing" aria-label="individual tour date listing">
                 <h3 className="tour-city">BROOMFIELD, CO</h3>
                 <p className="tour-date">March 23, 2025</p>
                 <p className="tour-location">Broomfield Auditorium</p>
-                <button className="tour-button-style">LEARN MORE</button>
+                <a 
+                    className="tour-button-style" 
+                    target='_blank'
+                >
+                    <button className="tour-button">LEARN MORE</button>
+                </a>
             </div>
             <hr className='listing-line'></hr>
         </div>
