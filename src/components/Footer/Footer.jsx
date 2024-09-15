4d9793550ddc40294e13fa0512bf89deea834605
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-09-14 19:03:53 -0600

    Finish overlay layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,42 +10,42 @@ function Footer() {
 
     return (
         <footer>
-            <hr className='orange-line-footer'></hr>  
+            <hr className="orange-line-footer"></hr>  
             <div className="social-links" aria-label="Social media links">
-                <a className="facebook"
+                <a className="link"
                     href="https://www.facebook.com/people/LAYLA-and-other-assorted-Eric-Clapton-songs-Musical-Showcase/61562647367534/"
-                    target='_blank'
+                    target="_blank" 
+                    aria-label="Facebook profile link"
                 >
                     <img 
-                        className="link"
+                        className="icon"
                         src={facebook}
                         onMouseEnter={e => e.currentTarget.src = facebookHov}
                         onMouseOut={e => e.currentTarget.src = facebook}
-                        alt="Production group facebook profile link"
                     />
                 </a>
                 <a className="link" 
                     href="https://www.youtube.com/channel/UCV9foKeW3pNIOOCOqq6_8Yg"
                     target='_blank'
+                    aria-label="Youtube profile link"
                 >
                     <img 
                         className="icon"
                         src={youtube}
                         onMouseEnter={e => e.currentTarget.src = youtubeHov}
                         onMouseOut={e => e.currentTarget.src = youtube}
-                        alt="Production group youtube profile link"
                     />
                 </a>
-                <a className="instagram" 
+                <a className="link" 
                     href="" 
                     target='_blank'
+                    aria-label="Instagram profile link"
                 >
                     <img 
-                        className="link"
+                        className="icon"
                         src={instagram}
                         onMouseEnter={e => e.currentTarget.src = instagramHov}
                         onMouseOut={e => e.currentTarget.src = instagram}
-                        alt="Production group instagram profile link"
                     />
                 </a>
             </div>
--- src/components/HamburgerNav/HBNavOverlay.jsx
+++ src/components/HamburgerNav/HBNavOverlay.jsx
@@ -1,4 +1,10 @@
 import { HashLink } from 'react-router-hash-link'
+import facebook from '../../assets/facebook.svg'
+import facebookHov from '../../assets/facebook-hover.svg'
+import instagram from '../../assets/instagram.svg'
+import instagramHov from '../../assets/instagram-hover.svg'
+import youtube from '../../assets/youtube.svg'
+import youtubeHov from '../../assets/youtube-hover.svg'
 import './HBNavOverlay.css'
 
 function HBNavOverlay() {
@@ -9,7 +15,49 @@ function HBNavOverlay() {
                 <HashLink className="hb-nav-link" to="/#about">about</HashLink>
                 <HashLink className="hb-nav-link" to="/#cast">cast</HashLink>
                 <a className="hb-nav-link" href="/faq">FAQ</a>
-                <HashLink className="tour-btn" to="/#dates">TOUR DATES</HashLink>
+
+                <div className="hb-social-links" aria-label="Social media links">
+                    <a 
+                        className="hb-link"
+                        href="https://www.facebook.com/people/LAYLA-and-other-assorted-Eric-Clapton-songs-Musical-Showcase/61562647367534/"
+                        target='_blank'
+                    >
+                        <img 
+                            className="hb-icon"
+                            src={facebook}
+                            onMouseEnter={e => e.currentTarget.src = facebookHov}
+                            onMouseOut={e => e.currentTarget.src = facebook}
+                            alt="Production group facebook profile link"
+                        />
+                    </a>
+                    <a 
+                        className="hb-link" 
+                        href="https://www.youtube.com/channel/UCV9foKeW3pNIOOCOqq6_8Yg"
+                        target='_blank'
+                    >
+                        <img 
+                            className="hb-icon"
+                            src={youtube}
+                            onMouseEnter={e => e.currentTarget.src = youtubeHov}
+                            onMouseOut={e => e.currentTarget.src = youtube}
+                            alt="Production group youtube profile link"
+                        />
+                    </a>
+                    <a 
+                        className="hb-link" 
+                        href="" 
+                        target='_blank'
+                    >
+                        <img 
+                            className="hb-icon"
+                            src={instagram}
+                            onMouseEnter={e => e.currentTarget.src = instagramHov}
+                            onMouseOut={e => e.currentTarget.src = instagram}
+                            alt="Production group instagram profile link"
+                        />
+                    </a>
+                </div>
+                <HashLink className="hb-tour-btn" to="/#dates">TOUR DATES</HashLink>
             </div>
         </nav>
     )
