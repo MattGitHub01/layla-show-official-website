1056ec333444ff1896631676cfc1989e455fecf9
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-09-17 14:54:04 -0600

    Transfer form data to .txt file and automatically download it, working on turning into an email

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,25 @@
+import { useState } from 'react'
 import Header from '../Header/Header.jsx'
 import Footer from '../Footer/Footer.jsx'
+import func from './formData.js'
 import './Form.css'
 
-function Form() {
+function Form() { // Input IDs: name, email, subject, message
+    const [formData, setFormData] = useState('');
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        let dataObjCons = new FormData(e.target);
+        let formDataObj = Object.fromEntries(dataObjCons.entries());
+        setFormData(formDataObj);
+        func(formData);
+    }
+
 
     return(
-        <>
+        <> 
             <Header />
-            <form className="contact-form" action="POST" aria-label="Contact the band using this form">
+            <form className="contact-form" onSubmit={onSubmit} action="#" aria-label="Contact the band using this form">
                 <label className="form-label" htmlFor="name">Your Name (required)</label>
                 <input className="form-input" type="text" id="name" name="name" required></input>
                 <label className="form-label" htmlFor="email">Your Email (required)</label>
@@ -16,7 +28,7 @@ function Form() {
                 <input className="form-input" type="text" id="subject" name="subject"></input>
                 <label className="form-label" htmlFor="message">Your Message</label>
                 <textarea className="form-msg" id="message" name="message" cols={5} rows={10}></textarea>
-                <input className="form-submit" type="submit" value="Submit"></input>
+                <input className="form-submit" type="submit" value="Submit" id="submit-btn"></input>
             </form>
             <Footer />
         </>
---
+++ src/components/Form/formData.js
@@ -0,0 +1,36 @@
+let formText = '';
+
+function download(filename) {
+    let data = formText;
+    let type = 'text/plain';
+    let file = new Blob([data], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        let a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+}
+
+function formData(data) {
+    const dataName = data.name;
+    const dataEmail = data.email;
+    const dataSubject = data.subject;
+    const dataMessage = data.message;
+    const textName = dataName.toString();
+    const textEmail = dataEmail.toString();
+    const textSubject = dataSubject.toString();
+    const textMessage = dataMessage.toString();
+    formText = `${textName} ${textEmail} ${textSubject} ${textMessage}`;
+    download('form-input.txt')
+}
+
+export default formData
