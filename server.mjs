981f5b34839bf57c1eae04897c95517d53ec7c17
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-09-20 03:12:48 -0600

    Set up backend sever.mjs, configure to run front end website

--- server.mjs
+++ server.mjs
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Serve static files from React app build/dist folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
 // Handle GET requests to / and serve the index.html dist/build file
 app.use('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
