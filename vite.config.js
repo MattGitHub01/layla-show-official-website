c4036efc3821dde4e2e7b70d1d9c3b3ca29d0c71
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-09-20 08:30:41 -0600

    Update for railway.app demo

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
         '/api': {
-            target: 'http://localhost:3000', // Change this to your Express server URL
+            target: 'http://localhost:3000',
             changeOrigin: true,
             rewrite: (path) => path.replace(/^\/api/, ''),
         },
