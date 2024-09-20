69acda1daece07d419877185785fb1c84e492437
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-09-20 03:10:29 -0600

    Code server.mjs

---
+++ server.mjs
@@ -0,0 +1,17 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Get __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Handle GET requests to / and serve the index.html dist/build file
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
