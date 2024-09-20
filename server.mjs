c3d6843659dd195a1a678bd7ab3336efc9f1e40b
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-09-20 05:32:19 -0600

    Finish writing code to handle ReCaptcha in server.mjs

--- server.mjs
+++ server.mjs
@@ -19,11 +19,31 @@ app.use(express.static(path.join(__dirname, 'dist')));
 // Parse JSON
 app.use(express.json());
 
-// Handle POST requests to verrify Google ReCAPTCHA
+// Handle POST requests to verify Google ReCAPTCHA
 app.post('/api/verify-captcha', async (req, res) => {
     const { captcha } = req.body;
     const SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY;
-})
+
+    try {
+        const response = await axios.post(`https://www.google.com/recaptcha/api/siteverify`, null, {
+            params: {
+                secret: SECRET_KEY,
+                response: captcha,
+            },
+        });
+
+        const { success } = response.data;
+
+        if (success) {
+            return res.status(200).json({ message: "Success! CAPTCHA verified!"});
+        } else {
+            return res.status(400).json({ message: "Failed! CAPTCHA not verified!"});
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Server error"});
+    }
+});
 
 // Handle GET requests to / and serve the index.html dist/build file
 app.use('*', (req, res) => {
